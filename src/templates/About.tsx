81eeca78c155c4b6a9fbf22abd0c73a923612033
Edward <e.arechwa@gmail.com>
2021-04-18 20:04:40 +0200

    feat: add function for split title for letter

--- src/templates/About.tsx
+++ src/templates/About.tsx
@@ -2,31 +2,51 @@ import { graphql } from "gatsby";
 import * as React from "react";
 import Layout from "../components/layout";
 
-const About = ({data}) => {
+const formatTitle = title => {
+  const titleArr = title.split(" ");
+  if (titleArr.length > 1) {
+    return titleArr.map((item, index) => {
+      const titleLetters = item.split("").map((item, index) => {
+        return (
+          <span className="title__letter title__letter--scale" key={index}>
+            {item}
+          </span>
+        );
+      });
+      return (
+        <span
+          className="row  animate__animated animate__rollIn animate__delay-0"
+          key={index}
+        >
+          {titleLetters}
+        </span>
+      );
+    });
+  } else {
+    const letters = titleArr.split("").map((item, index) => {
+      return (
+        <span className="title__letter title__letter--scale" key={index}>
+          {item}
+        </span>
+      );
+    });
+    return (
+      <span className="row  animate__animated animate__rollIn animate__delay-0">
+        {letters}
+      </span>
+    );
+  }
+};
+
+const About = ({ data }) => {
   const fields = data.mdx.frontmatter;
   console.log(fields.title);
-  const title = fields.title.split(' ').map((item, index) => {
-    return <span className="title__letter title__letter--scale" key={index}>{item}</span>
-  })
+  const title = formatTitle(fields.title);
   console.log(title);
   return (
     <Layout>
       <section className="about container">
-        <h2 className="title title--main title--about about__title">
-          {title}
-          <span className="row  animate__animated animate__rollIn animate__delay-0">
-            <span className="title__letter title__letter--scale">a</span>
-            <span className="title__letter title__letter--scale">b</span>
-            <span className="title__letter title__letter--scale">o</span>
-            <span className="title__letter title__letter--scale">u</span>
-            <span className="title__letter title__letter--scale">t</span>
-          </span>
-          <span className="row  animate__animated animate__rollIn animate__delay-1">
-            {" "}
-            <span className="title__letter title__letter--scale">m</span>
-            <span className="title__letter title__letter--scale">e</span>
-          </span>
-        </h2>
+        <h2 className="title title--main title--about about__title">{title}</h2>
         <div className="about__wrapper">
           <p className="text text--about about__text animate__animated animate__rollIn animate__delay-2">
             {fields.text1}
@@ -42,15 +62,15 @@ const About = ({data}) => {
 };
 
 export const query = graphql`
-query($slug: String!) {
-  mdx(frontmatter: {slug: {eq: $slug}}) {
-    frontmatter {
-      title
-      text1
-      text2
+  query($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        text1
+        text2
+      }
     }
   }
-}
-`
+`;
 
 export default About;
